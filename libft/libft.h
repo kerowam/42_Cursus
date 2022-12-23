0daa07467652dee419d31cb1054df14bf89b7f27
Germán Fredes <105356789+kerowam@users.noreply.github.com>
2022-12-23 22:40:09 +0100

    Libft project until ft_itoa (unfinished)

---
+++ libft/libft.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfredes- <gfredes-@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/04 16:45:44 by gfredes-          #+#    #+#             */
+/*   Updated: 2022/12/19 20:35:12 by gfredes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+# include <stdlib.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *dest, int c, size_t len);
+void	ft_bzero(void *dest, size_t n);
+void	*ft_memcpy(void *dest, const void *sorc, size_t len);
+void	*ft_memmove(void *dest, const void *sorc, size_t len);
+size_t	ft_strlcpy(char *dest, const char *sorc, size_t size);
+size_t	ft_strlcat(char *dest, const char *sorc, size_t size);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_atoi(const char *str);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strtrim(const char *s1, const char *set);
+char	**ft_split(const char *s, char c);
+char	*ft_itoa(int n);
+
+#endif

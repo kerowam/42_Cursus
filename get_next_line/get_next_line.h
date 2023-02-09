2a53f8cab340265059cbdb161ca0f01f322dd5af
Germán Fredes <germanfredes1988@protonmail.com>
2023-02-09 14:34:19 +0100

    Utils included in get_next_line

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -10,3 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char		*get_next_line(int fd);
+char		*ft_strjoin(const char *s1, const char *s2);
+char		*ft_strchr(char *s, int c);
+size_t	ft_strlen(const char *s);
+
+#endif
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -10,3 +10,65 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	size_t	i;
+	size_t	j;
+	char		*str;
+
+	str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str)
+		return (0);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	int	count;
+
+	count = 0;
+	if ((char)c == '\0')
+	{
+		while (s[count] != '\0')
+			count++;
+		return ((char *)&s[count]);
+	}
+	while (s[count] != '\0')
+	{
+		if (s[count] == (char)c)
+		{
+			return ((char *)&s[count]);
+		}
+		count++;
+	}
+	return (0);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	int	c;
+
+	c = 0;
+	while (*s != '\0')
+	{
+		s++;
+		c++;
+	}
+	return (c);
+}

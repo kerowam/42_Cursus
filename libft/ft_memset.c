cfc1c941d7137965f5722d2af669800524d973a3
gfredes- <germanfredes1988@protonmail.com>
2023-04-08 18:39:56 +0200

    libft updated to include ft_printf

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,9 +6,10 @@
 /*   By: gfredes- <gfredes-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 23:49:37 by gfredes-          #+#    #+#             */
-/*   Updated: 2022/12/08 00:10:09 by gfredes-         ###   ########.fr       */
+/*   Updated: 2023/04/08 18:29:07 by gfredes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memset(void *dest, int c, size_t len)
--- libft/ft_printf.c
+++ libft/ft_printf.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "libft.h"
 
 static void ft_print_c(char c, size_t *len)
 {
